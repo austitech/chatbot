ca5ca3c4ef3e7fbd57e73db5ae50f551f16ec4a1
Josiah Augustine <josiah.augustine.o@gmail.com>
2020-07-28 15:10:25 +0100

    resolved rebase conflict

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, render_template, request
+
+from chatterbot import ChatBot
+
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+# create flask app object
+app = Flask(__name__)
+
+# create chatbot
+bot = ChatBot(
+    "HNGi7-Bot",
+    logic_adapters=["chatterbot.corpus.english.conversations"])
+
+# configure chatbot
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train(
+    "chatterbot.corpus.english.greetings"
+)
+
+
+# create flask routes
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    user_text = request.args.get("msg")
+    return str(bot.get_response(user_text))
+
+
+if __name__ == "__main__":
+    app.run()
